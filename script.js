80c9866918d8295b5c67ede30d34cad0b570940f
Giadaspt <saputo.giada@gmail.com>
2021-11-09 14:13:05 +0100

    primo commit

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+
+1. creo una funzione che mi genera 5 numeri random con ciclo 
+2. faccio stampare i numeri all interno del quadrato apposito e lo faccio o con html o con js scrivendo a mano l html 
+3. setto l intervallo per tot secondi e dico che appena passati quesi secondi partono i prompt
+4. se il valore dei prompt è == al numero random hai indovinato tot altrimenti nessuno
+*/
